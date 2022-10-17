971e8968104cab5de85a277680c17b62a8d8e3d8
Kitty <73242220+Richard382@users.noreply.github.com>
2022-10-18 06:56:30 +1000

    Update Root.jsx

--- src/Root.jsx
+++ src/Root.jsx
@@ -3,10 +3,8 @@ import { Component } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { Web3ContextProvider } from "./hooks/web3Context";
-
 import App from "./App";
 import store from "./store";
-
 export default class Root extends Component {
   constructor(props) {
     super(props);
